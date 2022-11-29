12f26af8d2204698940f821ad2266801e75c0197
john@home <johninquest@gmail.com>
2022-11-30 00:26:38 +0100

    nada

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<h1 class="head">Welcome home</h1>
+
+<div class="foot"><span>&copy</span>&nbsp;<a href={_url} target="_blank" rel="noreferrer">John Apps</a>&nbsp;{_mom}</div>
+
+<script>
+    let _mom = new Date().getFullYear();
+    let _url = 'https://johnapps.de';
+</script>
+
+<style>
+    .head {
+        text-align: center; 
+        margin-top: 6rem; 
+        color: blue;
+    }
+
+    .foot {
+        text-align: center;
+        font-weight: bold;
+        margin: 3rem auto 1rem auto;
+        color: teal;
+    }
+    a {text-decoration: none; color: teal;}
+</style>
