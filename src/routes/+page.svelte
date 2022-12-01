ec4f0c43a5835bceb37eb19f3663499fbe614d43
john@home <johninquest@gmail.com>
2022-12-01 01:27:26 +0100

    added basic navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,45 @@
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-<h1 class="head">Welcome home</h1>
-
-<div class="foot"><span>&copy</span>&nbsp;<a href={_url} target="_blank" rel="noreferrer">johnapps</a>&nbsp; <span>{_mom}</span> </div>
-
 <script>
-    let _mom = new Date().getFullYear();
-    let _url = 'https://johnapps.de';
+  import Button, { Label } from "@smui/button";
+  import { goto } from "$app/navigation";
+  let _mom = new Date().getFullYear();
+  let _url = "https://johnapps.de";
+  const goToContact = function () {
+    goto("/contact");
+  };
 </script>
 
-<style lang="scss">
-    .head {
-        text-align: center; 
-        margin: 6rem auto 6rem auto; 
-        color: var(--primary-color);
-    }
+<h1 class="head">Welcome home</h1>
+<div style="text-align: center; margin: 1rem">
+  <Button on:click={() => goToContact()} variant="raised">
+    <Label>Go</Label>
+  </Button>
+  <p style="margin: 1rem;">
+    <a href="/contact">Go to contact</a>
+  </p>
+</div>
 
-    .foot {
-        text-align: center;
-        font-weight: bold;
-        text-transform: uppercase;
-        margin: 3rem auto 3rem auto;
-        color: var(--txt-color); 
-        a {
-        text-decoration: none; 
-        color: var(--txt-color);
-        }
+<div class="foot">
+  <span>&copy</span>&nbsp;<a href={_url} target="_blank" rel="noreferrer"
+    >johnapps</a
+  >&nbsp; <span>{_mom}</span>
+</div>
+
+<style lang="scss">
+  .head {
+    text-align: center;
+    margin: 6rem auto 6rem auto;
+    color: var(--primary-color);
+  }
+
+  .foot {
+    text-align: center;
+    font-weight: bold;
+    text-transform: uppercase;
+    margin: 3rem auto 3rem auto;
+    color: var(--txt-color);
+    a {
+      text-decoration: none;
+      color: var(--txt-color);
     }
-   
+  }
 </style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,13 @@
 <div style="text-align: center; margin: 3rem">Contact page</div>
+<div class="btn">
+  <button on:click={() => history.back()}>Go back</button>
+  <p style="margin: 1rem;">
+    <a href="/">Back to startpage</a>
+  </p>
+</div>
+
+<style lang="scss">
+  .btn {
+    text-align: center;
+  }
+</style>
